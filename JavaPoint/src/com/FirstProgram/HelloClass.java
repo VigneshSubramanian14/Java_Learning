031d6765f68abbd50b3ff9fd9b743da1ceeb7c5f
b.g.balasubramanian <b.g.balasubramanian@CDC2-L-HYBRMW2.dir.svc.accenture.com>
2020-11-21 11:32:58 +0530

    This is a newly created project

---
+++ JavaPoint/src/com/FirstProgram/HelloClass.java
@@ -0,0 +1,15 @@
+package com.FirstProgram;
+
+public class HelloClass {
+
+	public static void method1() {
+		System.out.println("This is the first method created in the program");
+	}
+
+	static public void main(String args[]) {
+		System.out.println("This is the first program");
+		System.out.println("We are going to learn java");
+		method1();
+	}
+
+}
---
+++ JavaPoint/src/com/Randomdata/RandomTestData.java
@@ -0,0 +1,15 @@
+package com.Randomdata;
+
+import java.util.Random;
+
+public class RandomTestData {
+
+	public static void main(String[] args) {
+	
+		Random data = new Random();
+		
+		System.out.println("The random data is"+data.nextInt());
+
+	}
+
+}
